4b87e3a6c7c6e9aaafad132b6022b365d25114b9
Tosin-Adesina <oluwatosin0084@gmail.com>
2022-04-06 16:49:11 -0400

    debug deploy 1

--- prof'stemplate/auth.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose'),
-    passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy,
-    Player = mongoose.model('Player');
-
-passport.use(new LocalStrategy(Player.authenticate()));
-
-passport.serializeUser(Player.serializeUser());
-passport.deserializeUser(Player.deserializeUser());
